90cfc8cdfbe1aae5fb839d94062d36d6240d964d
Arez <iarezik@gmail.com>
2016-12-07 23:14:01 +0200

    fix bug in template with avatar

--- arez_pet/settings.py
+++ arez_pet/settings.py
@@ -94,6 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (STATIC_PATH,)
 
 MEDIA_PATH = os.path.join(BASE_DIR, 'media', )
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
